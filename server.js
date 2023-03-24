b8f2563436b44346c5a3ce5c3df79ec90de177a4
= <=>
2023-03-24 14:33:06 -0500

    add build command

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-// const express = require('express');
 import express from 'express';
 import cors from 'cors';
 const app = express();
-// var cors = require('cors')
 
 const port = process.env.PORT || 3000;
 app.options('*', cors())

6b86424913165221670cb381833a688ec75a5770
= <=>
2023-03-26 01:29:46 -0500

    fix: fix marker overlaping

--- assets/ui.js
+++ assets/ui.js
@@ -4,28 +4,38 @@ export class UiContext {
 
   makePoint(scene, props) {
     //GUI
-    let meshT = new BABYLON.Mesh("box", scene);
-    let modal = document.getElementById('myModal');
-    let advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
-    let markerImage = new BABYLON.GUI.Image(
-      "marker",
-      props.markerImg.url
-    );
-    markerImage.width = "32px";
-    markerImage.height = "32px";
-    markerImage.stretch = BABYLON.GUI.Image.STRETCH_UNIFORM;
-    //Modal Validation with template
-    if(props.hasOwnProperty('modal')){
-      markerImage.onPointerUpObservable.add(() => {
-        modal.innerHTML = props.modal.template;
-        modal.style.opacity = 1;
+    let advancedTextureMarker = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
+    let markerContainer = new BABYLON.GUI.Container();
+    advancedTextureMarker.addControl(markerContainer);
+    advancedTextureMarker.isPointerBlocker = false;
+    
+    BABYLON.SceneLoader.ImportMesh("", "/assets/textures/map_point/", "scene.gltf", scene, function onSuccess(meshes) {
+      let markerMesh = meshes[0];
+      markerMesh.isVisible = false;
+      markerMesh.actionManager = new BABYLON.ActionManager(scene);
+      markerMesh.position.y = 10;
+    
+      // Crear imagen GUI para el marcador
+      let markerImage = new BABYLON.GUI.Image("marker");
+      markerImage.stretch = BABYLON.GUI.Image.STRETCH_UNIFORM;
+      markerContainer.addControl(markerImage);
+      markerImage.billboardMode = BABYLON.AbstractMesh.BILLBOARDMODE_ALL;
+      markerContainer.linkWithMesh(markerMesh);
+    
+      markerImage.onPointerUpObservable.add(function () {
+        // modal.style.display = "block";
       });
-    }
-    advancedTexture.addControl(markerImage);
-    markerImage.linkWithMesh(meshT);
-    markerImage.billboardMode = BABYLON.AbstractMesh.BILLBOARDMODE_ALL;
+    
+      markerMesh.position = new BABYLON.Vector3(props.position.x, props.position.y, props.position.z);
 
-    meshT.position = new BABYLON.Vector3(props.position.x, props.position.y, props.position.z);
+    }, null, function onError(scene, message, exception) {
+      try {
+          console.error("Error cargando archivo GLTF:", message, exception);
+          console.log("Valor de t:", t);
+      } catch (error) {
+          console.error("Error en la función onError:", error);
+      }
+    });
 
     let pickCylinder = function (meshEvent) {
       // modal.style.display = "block";
